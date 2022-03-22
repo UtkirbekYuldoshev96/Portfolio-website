ed31c63b68791a71f3bf1d113de0a6bbb2d700b5
UtkirbekYuldoshev96 <91976877+UtkirbekYuldoshev96@users.noreply.github.com>
2022-03-22 13:41:00 +0500

    Portfolio-website

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/*=============== SHOW MENU ===============*/
+
+/*============== MENU HIDDEN ===============*/
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+let swiper = new Swiper(".testimonial-wrapper", {
+    Loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+// var swiper = new Swiper(".testimonial-wrapper", {
+//     direction: "vertical",
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+// });
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== PORTFOLIO ITEM FILTER ===============*/
+
+/*=============== THEME/DISPLAY CUSTOMIZATION ===============*/
+
+/*===== FONTS =====*/
+
+/*===== PRIMARY COLORS =====*/
+
+/*===== THEME BACKGROUNDS =====*/
